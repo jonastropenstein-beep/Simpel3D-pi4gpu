5b074a16130b5fb84cb8b93a2b15f196698101d4
jonastropenstein-beep <jonastropenstein@gmail.com>
2026-07-25 19:34:51 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+echo "Starte Installation von Simpel3D-pi4gpu..."
+sudo apt update
+sudo apt install -y python3
+echo "Kopiere GPU-Konfiguration..."
+sudo cp gpu.conf /etc/simpel3d.conf
+echo "Starte Treiber..."
+python3 driver.py
+echo "Installation abgeschlossen!"
