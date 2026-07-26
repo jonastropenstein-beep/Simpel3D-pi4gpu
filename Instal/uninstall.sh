011a6b436f6f05ecb8ee0cdc7ccfa6e2317cdfcb
zoviloplay <jonastropenstein@gmail.com>
2026-07-26 10:57:44 +0200

    Update uninstall.sh

--- Instal/uninstall.sh
+++ Instal/uninstall.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+echo "Deinstalliere Simpel3D-pi4gpu..."
 
+sudo rm -rf /opt/simpel3d-pi4gpu
+sudo rm -f /usr/local/bin/simpel3d-pi4gpu
+rm -f ~/Desktop/Simpel3D-pi4gpu.desktop
+
+echo "Simpel3D-pi4gpu wurde erfolgreich entfernt."
